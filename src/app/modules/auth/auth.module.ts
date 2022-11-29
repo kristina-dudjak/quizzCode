374d6aaef7bc7f35f5e47d81e7265a753c0029f6
Kristina Dudjak <dudjakkristina@gmail.com>
2022-11-29 23:01:01 +0100

    feat: implement login design

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,19 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { RegisterComponent } from './views/register/register.component';
-import { LoginComponent } from './views/login/login.component';
+import { AuthRoutingModule } from './auth-routing.module'
+import { RegisterComponent } from './views/register/register.component'
+import { LoginComponent } from './views/login/login.component'
 
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 
 @NgModule({
-  declarations: [
-    RegisterComponent,
-    LoginComponent
-  ],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatDialogModule,
+    MatCheckboxModule
   ]
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/modules/auth/views/login/login.component.ts
+++ src/app/modules/auth/views/login/login.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, Validators } from '@angular/forms'
+import { PasswordRegex } from 'src/app/shared/const/PasswordRegex'
+import { ValidationService } from '../../services/validation.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor (
+    private validationService: ValidationService,
+    private fb: FormBuilder
+  ) {}
 
-  constructor() { }
+  loginForm = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(PasswordRegex)
+        ]
+      ]
+    },
+    {
+      validators: [
+        this.validationService.validate('email'),
+        this.validationService.validate('password')
+      ],
+      updateOn: 'change'
+    }
+  )
 
-  ngOnInit(): void {
+  isPasswordVisible = true
+  rememberMe = true
+
+  onLogin () {
+    if (!this.loginForm.valid) return
   }
 
+  onGoogleLogin () {}
+
+  openEmailDialog () {}
 }
