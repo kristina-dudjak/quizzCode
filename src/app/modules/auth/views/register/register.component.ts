7bd201d2c5959ca3e3a197d00511dab36a891b9e
Kristina Dudjak <dudjakkristina@gmail.com>
2022-11-29 23:07:35 +0100

    feat: implement register design

--- src/app/modules/auth/views/register/register.component.ts
+++ src/app/modules/auth/views/register/register.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, Validators } from '@angular/forms'
+import { PasswordRegex } from 'src/app/shared/const/PasswordRegex'
+import { ValidationService } from '../../services/validation.service'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  constructor (
+    private validationService: ValidationService,
+    private fb: FormBuilder
+  ) {}
 
-  constructor() { }
+  registerForm = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(PasswordRegex)
+        ]
+      ],
+      repeatPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(PasswordRegex)
+        ]
+      ]
+    },
+    {
+      validators: [
+        this.validationService.validate('email'),
+        this.validationService.validate('password'),
+        this.validationService.validate('repeatPassword'),
+        this.validationService.validatePasswordMatch('repeatPassword')
+      ],
+      updateOn: 'change'
+    }
+  )
 
-  ngOnInit(): void {
+  isPasswordVisible = true
+  isPasswordRepeatVisible = true
+  rememberMe = true
+
+  onRegister () {
+    if (!this.registerForm.valid) {
+      return
+    }
   }
 
+  onGoogleLogin () {}
 }
