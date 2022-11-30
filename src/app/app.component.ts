f6380a86181326e9b50a686924d2b38f13447e73
Kristina Dudjak <dudjakkristina@gmail.com>
2022-11-30 14:35:38 +0100

    feat: add icons service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { IconsService } from './shared/services/icons.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor (private iconsService: IconsService) {}
+
+  ngOnInit () {
+    this.iconsService.addIcons()
+  }
+}
