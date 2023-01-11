383f5d7be55f1f74e865301671940064c4add693
Kristina Dudjak <dudjakkristina@gmail.com>
2023-01-11 23:42:17 +0100

    feat: new quiz style template

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common'
 import { AdminRoutingModule } from './admin-routing.module'
 import { MatInputModule } from '@angular/material/input'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
 import { AdminComponent } from './views/admin/admin.component'
 import { AdminDefaultComponent } from './components/admin-default/admin-default.component'
@@ -24,7 +27,10 @@ import { AnswersInputComponent } from './components/answers-input/answers-input.
     AdminRoutingModule,
     MatInputModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class AdminModule {}
